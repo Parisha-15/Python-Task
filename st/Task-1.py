559d5dcc681e83e4f00184b0de53a1cfc0cd77d5
Parisha-15 <parishamalvania315@gmail.com>
2026-01-08 13:26:51 +0530

    Add files via upload

--- st/Task-1.py
+++ st/Task-1.py
@@ -3,18 +3,20 @@ st.sidebar.title("Std info")
 b=st.sidebar.selectbox("Select Batch",["B1","B2","B3","B4"])
 ge=st.sidebar.radio("Select Gender",["Male","Female"])
 x=st.sidebar.selectbox("Select Subject",["FSD","Python"])
-if x=="Python":
-    st.write("Welcome to Python")
-    name=st.sidebar.text_input("Enter your Name: ")
-    en=st.sidebar.number_input("Enter your enrollement number: ")
-    project_title=st.sidebar.text_input("ENter title:")
-    project_desc=st.sidebar.text_area("Enter description")
+tab1,tab2=st.tab("Python","FSD")
+if tab1==x[0]:
+    if x=="Python":
+        st.write("Welcome to Python")
+        name=st.sidebar.text_input("Enter your Name: ")
+        en=st.sidebar.number_input("Enter your enrollement number: ")
+        project_title=st.sidebar.text_input("ENter title:")
+        project_desc=st.sidebar.text_area("Enter description")
 else:
-    st.write("welcome to FSD")
-    name=st.sidebar.text_input("Enter your Name: ")
-    en=st.sidebar.number_input("Enter your enrollement number: ")
-    project_title=st.sidebar.text_input("ENter title:")
-    project_desc=st.sidebar.text_area("Enter description")
+        st.write("welcome to FSD")
+        name=st.sidebar.text_input("Enter your Name: ")
+        en=st.sidebar.number_input("Enter your enrollement number: ")
+        project_title=st.sidebar.text_input("ENter title:")
+        project_desc=st.sidebar.text_area("Enter description")
 btn=st.sidebar.button("Submit")
 if btn==True:
     st.write(name)
@@ -25,3 +27,4 @@ if btn==True:
     st.write(project_title)
     st.write(project_desc)
 
+st.markdown("",unsafe_allow_html=True)
---
+++ st/Task-3.py
@@ -0,0 +1,42 @@
+import streamlit as st
+st.header("This is Task-3")
+x=st.file_uploader("Upload Photo",type=["pdf"])
+# if st.button("Upload File"):
+#     st.image(x)
+st.success("s")
+st.warning('w')
+st.info("i")
+st.error("e")
+if st.button("submit"):
+    x=st.progress(0)
+    with st.spinner("loading..."):
+        for i in range(100):
+            import time
+            time.sleep(0.1)
+            x.progress(i+1)
+    st.success("Submitted sucessfully")
+
+import pandas as pd
+data={"name":["A","B","C"],"age":[1,2,3]}
+df=pd.DataFrame(data)
+st.header('DataFrame')
+st.dataframe(df)
+
+st.header("Table")
+st.table(data)
+import matplotlib.pyplot as plt
+x=["A","B","C","D"]
+y=[20,10,15,25]
+plt.bar(x,y,width=0.4)
+st.pyplot(plt)
+plt.clf()
+plt.scatter(x,y)
+st.pyplot(plt)
+import numpy as np
+a=np.arange(1,11)
+b=np.random.randint(1,100,10)
+z=np.column_stack((a,b))
+st.line_chart(z)
+st.bar_chart(z)
+st.markdown("*Std*")
+st.write(code="HTML",<u>,)
